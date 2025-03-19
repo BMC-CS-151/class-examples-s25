1ec4a96766816027746e4f9d18637b8f817b25a5
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2025-03-19 14:11:50 -0400

    tree

---
+++ lec14/LinkedBinaryTreeImpl.java
@@ -0,0 +1,67 @@
+public class LinkedBinaryTreeImpl<E extends Comparable<E>> {
+    Node<E> root;
+
+    public LinkedBinaryTreeImpl() {
+        root = null;
+    }
+
+    public LinkedBinaryTreeImpl(E data) {
+        root = new Node(data);
+    }
+
+    private class Node<E> {
+        private E element;
+        private Node<E> left;
+        private Node<E> right;
+
+        public Node() {
+            //init empty tree
+            this.left = null;
+            this.right = null;
+        }
+
+        public Node(E data) {
+            this.element = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+    public void InOrderTraversal() {
+        InOrderTraversal(root);
+    }
+
+    private void InOrderTraversal(Node node) {
+        //base case
+        if (node == null) return;
+
+        InOrderTraversal(node.left);
+        System.out.println(node.element);
+        InOrderTraversal(node.right);
+    }
+
+    public Node search(E key) {
+        return search(root, key);
+    }
+
+    private Node search(Node node, E key) {
+        if (node == null) {
+            return null;
+        }
+
+        //CASE 1: we found it!
+        if (key.compareTo((E) node.element) == 0) {
+            return node;
+        }
+
+        //CASE 2: larger than
+        if (key.compareTo((E) node.element) > 0) {
+            return search(node.right, key);
+        } 
+
+        //(key.compareTo((E) node.element) < 0) {
+        //CASE 3: smaller than
+        return search(node.left, key);
+    }
+}
